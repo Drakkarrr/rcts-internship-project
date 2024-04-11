d6146a1a7a6443c191ed02be27add284b702fabb
Drakkarrr <drakkar.alpha00@gmail.com>
2024-04-10 18:20:57 -0700

    side content

--- client/src/modules/AuthModule/SideContent.jsx
+++ client/src/modules/AuthModule/SideContent.jsx
@@ -9,7 +9,7 @@ const { Title, Text } = Typography;
 
 export default function SideContent() {
   const translate = useLanguage();
-  const langDirection = useSelector(selectLangDirection)
+  const langDirection = useSelector(selectLangDirection);
 
   return (
     <Content
@@ -22,30 +22,56 @@ export default function SideContent() {
       className="sideContent"
     >
       <div style={{ width: '100%' }}>
-        <img
+        {/* <img
           src={logo}
-          alt="IDURAR ERP CRM"
+          alt="ResponSivCode Technology Solutions"
           style={{ margin: '0 auto 40px', display: 'block' }}
           height={63}
           width={220}
-        />
+        /> */}
+        <h1>RCTS Logo Here!</h1>
         <div className="space40"></div>
-        <Title level={3}>{translate('Manage your company with')} :</Title>
+        <Title level={3}>Brgy Managemnet System :</Title>
 
         <div className="space20"></div>
-        <ul className="list-checked" style={{paddingRight:0}}>
-          <li className={`list-checked-item ${langDirection === "rtl" ? "list-checked-item-right" : "list-checked-item-left"}`}>
+        <ul className="list-checked" style={{ paddingRight: 0 }}>
+          <li
+            className={`list-checked-item ${
+              langDirection === 'rtl' ? 'list-checked-item-right' : 'list-checked-item-left'
+            }`}
+          >
             <Space direction="vertical">
-              <Text strong>{translate('All-in-one tool')}</Text>
-
-              <Text>{translate('Run and scale your ERP CRM Apps')}</Text>
+              <Text strong>All-in-one tool</Text>
             </Space>
           </li>
 
-          <li className={`list-checked-item ${langDirection === "rtl" ? "list-checked-item-right" : "list-checked-item-left"}`}>
+          <li
+            className={`list-checked-item ${
+              langDirection === 'rtl' ? 'list-checked-item-right' : 'list-checked-item-left'
+            }`}
+          >
             <Space direction="vertical">
-              <Text strong>{translate('Easily add and manage your services')}</Text>
-              <Text>{translate('It brings together your invoice clients and leads')}</Text>
+              <Text strong>Fully Automated</Text>
+            </Space>
+          </li>
+
+          <li
+            className={`list-checked-item ${
+              langDirection === 'rtl' ? 'list-checked-item-right' : 'list-checked-item-left'
+            }`}
+          >
+            <Space direction="vertical">
+              <Text strong>Secures your data</Text>
+            </Space>
+          </li>
+
+          <li
+            className={`list-checked-item ${
+              langDirection === 'rtl' ? 'list-checked-item-right' : 'list-checked-item-left'
+            }`}
+          >
+            <Space direction="vertical">
+              <Text strong>User friendly</Text>
             </Space>
           </li>
         </ul>
@@ -55,68 +81,7 @@ export default function SideContent() {
             display: 'flex',
             justifyContent: 'space-between',
           }}
-        >
-          {/* <img
-            src={logo1}
-            alt="Logo1"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo2}
-            alt="Logo2"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo3}
-            alt="Logo3"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo4}
-            alt="Logo4"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          /> */}
-        </div>
+        ></div>
       </div>
     </Content>
   );
