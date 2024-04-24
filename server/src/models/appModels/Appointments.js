0fc283a38e3675273a8e75c2a68d0ff309014c3b
Drakkarrr <drakkar.alpha00@gmail.com>
2024-04-24 11:34:37 -0700

    requestor label changed

--- server/src/models/appModels/Appointments.js
+++ server/src/models/appModels/Appointments.js
@@ -10,74 +10,78 @@ const schema = new mongoose.Schema({
     default: true,
   },
 
-  type: {
-    type: String,
-    default: 'company',
-    enum: ['company', 'people'],
-    required: true,
-  },
+  requestor: { type: mongoose.Schema.ObjectId, ref: 'Requestor', autopopulate: true },
+
   name: {
     type: String,
     required: true,
   },
-  company: { type: mongoose.Schema.ObjectId, ref: 'Company', autopopulate: true },
-  people: { type: mongoose.Schema.ObjectId, ref: 'People', autopopulate: true },
-  interestedIn: [{ type: mongoose.Schema.ObjectId, ref: 'Product' }],
-  offer: [{ type: mongoose.Schema.ObjectId, ref: 'Offer' }],
-  converted: { type: Boolean, default: false },
-  createdBy: { type: mongoose.Schema.ObjectId, ref: 'Admin' },
-  assigned: { type: mongoose.Schema.ObjectId, ref: 'Admin' },
-  subTotal: {
-    type: Number,
-  },
-  taxTotal: {
-    type: Number,
-  },
-  total: {
-    type: Number,
-  },
-  discount: {
-    type: Number,
-  },
-  images: [
-    {
-      id: String,
-      name: String,
-      path: String,
-      description: String,
-      isPublic: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  ],
-  files: [
-    {
-      id: String,
-      name: String,
-      path: String,
-      description: String,
-      isPublic: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  ],
-  category: String,
-  status: String,
-  notes: String,
-  source: String,
-  approved: {
-    type: Boolean,
-    default: false,
-  },
-  tags: [
-    {
-      type: String,
-      trim: true,
-      lowercase: true,
-    },
-  ],
+
+  // type: {
+  //   type: String,
+  //   default: 'company',
+  //   enum: ['company', 'people'],
+  //   required: true,
+  // },
+
+  // company: { type: mongoose.Schema.ObjectId, ref: 'Company', autopopulate: true },
+  // people: { type: mongoose.Schema.ObjectId, ref: 'People', autopopulate: true },
+  // interestedIn: [{ type: mongoose.Schema.ObjectId, ref: 'Product' }],
+  // offer: [{ type: mongoose.Schema.ObjectId, ref: 'Offer' }],
+  // converted: { type: Boolean, default: false },
+  // createdBy: { type: mongoose.Schema.ObjectId, ref: 'Admin' },
+  // assigned: { type: mongoose.Schema.ObjectId, ref: 'Admin' },
+  // subTotal: {
+  //   type: Number,
+  // },
+  // taxTotal: {
+  //   type: Number,
+  // },
+  // total: {
+  //   type: Number,
+  // },
+  // discount: {
+  //   type: Number,
+  // },
+  // images: [
+  //   {
+  //     id: String,
+  //     name: String,
+  //     path: String,
+  //     description: String,
+  //     isPublic: {
+  //       type: Boolean,
+  //       default: false,
+  //     },
+  //   },
+  // ],
+  // files: [
+  //   {
+  //     id: String,
+  //     name: String,
+  //     path: String,
+  //     description: String,
+  //     isPublic: {
+  //       type: Boolean,
+  //       default: false,
+  //     },
+  //   },
+  // ],
+  // category: String,
+  // status: String,
+  // notes: String,
+  // source: String,
+  // approved: {
+  //   type: Boolean,
+  //   default: false,
+  // },
+  // tags: [
+  //   {
+  //     type: String,
+  //     trim: true,
+  //     lowercase: true,
+  //   },
+  // ],
   created: {
     type: Date,
     default: Date.now,
