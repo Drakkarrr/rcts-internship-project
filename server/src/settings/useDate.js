8bc36680f3426477cde8a5d0106213595e9ef7d1
Drakkarrr <drakkar.alpha00@gmail.com>
2024-05-14 09:50:43 -0700

    cleanup

--- server/src/settings/useDate.js
+++ server/src/settings/useDate.js
@@ -1,7 +1,7 @@
 const useDate = ({ settings }) => {
-  const { idurar_app_date_format } = settings;
+  const { rcts_app_date_format } = settings;
 
-  const dateFormat = idurar_app_date_format;
+  const dateFormat = rcts_app_date_format;
 
   return {
     dateFormat,
